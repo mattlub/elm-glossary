f33788d0ebebc0d5c467ab18b8e12eabb93ca18e
matt lubel <matt_lubel@hotmail.com>
2017-10-13 16:28:12 +0100

    tests for iContains

--- tests/Example.elm
+++ tests/Example.elm
@@ -3,34 +3,28 @@ module Example exposing (..)
 import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer, int, list, string)
 import Test exposing (..)
+import Helpers exposing (iContains)
 
 
 suite : Test
 suite =
-    describe "The String module"
-        [ describe "String.reverse"
-            -- Nest as many descriptions as you like.
-            [ test "has no effect on a palindrome" <|
+    describe "The Helper Functions"
+        [ describe "iContains"
+            [ test "should work on normal true cases" <|
                 \_ ->
-                    let
-                        palindrome =
-                            "hannah"
-                    in
-                        Expect.equal palindrome (String.reverse palindrome)
-
-            -- Expect.equal is designed to be used in pipeline style, like this.
-            , test "reverses a known string" <|
+                    iContains "te" "test"
+                        |> Expect.equal True
+            , test "should work on case insensitive true cases (1)" <|
                 \_ ->
-                    "ABCDEFG"
-                        |> String.reverse
-                        |> Expect.equal "GFEDCBA"
-
-            -- fuzz runs the test 100 times with randomly-generated inputs!
-            , fuzz string "restores the original string if you run it again" <|
-                \randomlyGeneratedString ->
-                    randomlyGeneratedString
-                        |> String.reverse
-                        |> String.reverse
-                        |> Expect.equal randomlyGeneratedString
+                    iContains "TeSt" "test"
+                        |> Expect.equal True
+            , test "should work on case insensitive true cases (2)" <|
+                \_ ->
+                    iContains "abc" "ABcdEF"
+                        |> Expect.equal True
+            , test "should work on false cases" <|
+                \_ ->
+                    iContains "false" "testString"
+                        |> Expect.equal False
             ]
         ]
